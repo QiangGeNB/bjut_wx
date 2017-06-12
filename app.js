114142ba751f77c3fb7a501d82e03e2eeb5e3129
liyan <609505217@qq.com>
2017-06-12 22:02:44 +0800

    collect mongodb

--- app.js
+++ app.js
@@ -5,10 +5,44 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+var mongoose = require('mongoose');
+var Student = require('./db/models/student')
+//var ws = require('./websocket_server.js')
+var expressSession = require('express-session')
+var mongoStore = require('connect-mongo')(expressSession)
+
+
 var index = require('./routes/index');
 var users = require('./routes/users');
+var apiRoutes = require('./routes/apiRoutes');
 
 var app = express();
+
+//设置session信息
+app.use(expressSession({
+    resave: false,
+    secret: 'SECRET',
+    saveUninitialized: true,
+    cookie: { maxAge: 60*10000 },
+    store: new mongoStore({
+        mongooseConnection: mongoose.connection
+    })
+}));
+
+//跨域访问设置
+app.all('*', function(req, res, next) {
+res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Headers", "X-Requested-With");
+res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+res.header("X-Powered-By",' 3.2.1')
+res.header("Content-Type", "application/json;charset=utf-8");
+next();
+});
+
+
+app.use('/api',apiRoutes);//使用apiroutes
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- db/schema/shop.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('../db.js')
-
-var ShopSchema = new mongoose.Schema({
-	Name:String
-})
-
-module.exports = mongoose.model('eg_shop',ShopSchema);
