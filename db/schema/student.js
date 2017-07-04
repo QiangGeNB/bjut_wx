4f279132d559c0e0e1cbd035f6b651e335e65249
liyan <609505217@qq.com>
2017-07-04 16:39:40 +0800

    change schema

--- db/schema/student.js
+++ db/schema/student.js
@@ -1,16 +1,24 @@
 var mongoose = require('../db.js')
 
 var StudentSchema = new mongoose.Schema({
-	openid:String,
-	session_key:String,
+	openid:String,//openid
+	session_key:String,//session_key
 	userInfo:String,
-	nickName:String,
-	avatarUrl:String,
-	gender:Number,
-	province:String,
-	city:String,
-	country:String,
-	language:String
+	nickName:String,//昵称
+	avatarUrl:String,//头像地址
+	gender:Number,//性别
+	province:String,//省份
+	city:String,//城市
+	country:String,//国家
+	language:String,//语言
+	studentID:String,//学号
+	enter_year:String,//入学年份
+	collection_activity:[{
+		activityID:Number//收藏活动编号
+	}],
+	activity:[{
+		activityID:Number//参与活动编号
+	}]
 })
 
 module.exports = mongoose.model('bjut_student',StudentSchema);
